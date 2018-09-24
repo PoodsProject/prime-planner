b7f20aa83ce4eddb950015144bbb2d980f8487cf
Jacob Caraballo <jacobcaraballo@knights.ucf.edu>
2018-09-24 17:32:03 -0400

    setup the Task object for database management

--- Prime Planner/Core/Objects/Task.swift
+++ Prime Planner/Core/Objects/Task.swift
@@ -7,15 +7,41 @@
 //
 
 import Foundation
+import CoreData
+
+enum TaskPriority: Int16 {
+	case none, low, medium, high
+}
 
 
-// because 'Task' is automagically created and declared by our data model, we can extend it as follows, adding any new functions and getters that may be needed.
-extension Task {
+// we are going to manually manage the Task object, thus requiring it to be manually created
+class Task: NSManagedObject {
+	
+	// @NSManaged means this Task object is managed by the core database
+	@NSManaged var name: String
+	@NSManaged var creationDate: Date
+	@NSManaged var dueDate: Date?
+	@NSManaged var note: String
+	
+	// we disabled automatic management here, because we want
+	// to manage this specific property ourselves
+	var priority: TaskPriority {
+		set {
+			JManaged.set(newValue.rawValue, forKey: "priority", inObject: self)
+		}
+		get {
+			let value = JManaged.get("priority", inObject: self) as! Int16
+			return TaskPriority(rawValue: value)!
+		}
+	}
 	
 	// here we create an convenience initializer that will insert a new Task with the given name into the database
 	convenience init(name: String) {
 		self.init(context: jcore.context)
 		self.name = name
+		self.creationDate = Date()
+		self.priority = .none
+		self.note = ""
 	}
 	
 	
