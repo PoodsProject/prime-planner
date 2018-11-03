d0832ab0b427f88b88bc96a340983121a7d34026
Mike <mdfillinger@knights.ucf.edu>
2018-11-03 17:36:44 -0400

    created AppTheme
    created AppTheme class with class function to access UIColor

---
+++ Prime Planner/AppTheme.swift
@@ -0,0 +1,14 @@
+//
+//  AppTheme.swift
+//  Prime Planner
+//
+//  Created by Mac User on 11/3/18.
+//  Copyright © 2018 Poods. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AppTheme {
+    class func color() -> UIColor{ return UIColor(red:0.60, green:1.0, blue: 0.60, alpha: 1.0) }
+}
